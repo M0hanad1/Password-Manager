24bd9aabb2d6272d4e60a5408804d4d99a928122
Mohanad <mohned.dabsha@gmail.com>
2021-10-15 09:07:17 +0200

    First commit

---
+++ data.py
@@ -0,0 +1,93 @@
+from os import path
+import json
+
+
+class Data:
+	def __init__(self, file_name: str) -> None:
+		self.file_name = file_name
+
+		if path.isfile(file_name) is False or path.getsize(file_name) == 0:
+			with open(file_name, 'w') as f:
+				json.dump({}, f, indent=4)
+
+	@staticmethod
+	def convert_to(string: str) -> list:
+		return [ord(i) for i in string]
+
+	@staticmethod
+	def convert_from(lst: list) -> str:
+		return "".join([chr(i) for i in lst])
+
+
+	def save(self, name: str, password: str) -> bool:
+		with open(self.file_name) as f:
+			data = json.load(f)
+
+			if name in data:
+				return False
+
+			data[name] = self.convert_to(password)
+
+			with open(self.file_name, 'w') as d:
+				json.dump(data, d, indent=4)
+
+		return True
+
+	def show(self) -> dict:
+		result = {}
+
+		with open(self.file_name) as f:
+			data = json.load(f)
+
+			for i in data:
+				result[i] = self.convert_from(data[i])
+
+		return result
+
+	def edit_name(self, old_name: str, new_name: str) -> bool:
+		with open(self.file_name) as f:
+			data = json.load(f)
+
+			if new_name in data or old_name not in data:
+				return False
+
+			data[new_name] = data[old_name]
+			del data[old_name]
+
+			with open(self.file_name, 'w') as d:
+				json.dump(data, d, indent=4)
+
+		return True
+
+	def edit_pass(self, name: str, password: str) -> bool:
+		with open(self.file_name) as f:
+			data = json.load(f)
+
+			if name not in data:
+				return False
+
+			data[name] = self.convert_to(password)
+
+			with open(self.file_name, 'w') as d:
+				json.dump(data, d, indent=4)
+
+		return True
+
+	def delete(self, name: str) -> bool:
+		with open(self.file_name) as f:
+			data = json.load(f)
+
+			if name in data:
+				del data[name]
+
+			else:
+				return False
+
+			with open(self.file_name, 'w') as d:
+				json.dump(data, d, indent=4)
+
+		return True
+
+	def remove_all(self) -> None:
+		with open(self.file_name, 'w') as f:
+			json.dump({}, f, indent=4)
---
+++ main.py
@@ -0,0 +1,60 @@
+from data import Data
+from os import path
+
+class Main:
+	def __init__(self, master_file, data_file):
+		self.master_file = Data(master_file)
+		self.data_file = Data(data_file)
+
+		if bool(self.master_file.show()) is False:
+			if bool(self.data_file.show()):
+				self.data_file.remove_all()
+
+	def main(self):
+		if bool(self.master_file.show()) is False:
+			print('You don\'t have a master please\nPlease write one')
+			master_password = input('Write your master password:\n')
+
+			if len(master_password) == 0:
+				print('\nPleae Write your master password\n')
+
+			self.master_file.save('master', master_password)
+			print(f'\nYour master password is {master_password}\n')
+			return self.master_change()
+
+		else:
+			master_password = input('Write your master password:\n')
+
+			if master_password == self.master_file.show()['master']:
+				print('\nWelcome\n')
+				return self.master_change()
+
+			else:
+				print('\nThat\'s not your master password\nPlease try again\n')
+
+		return self.main()
+
+	def master_change(self):
+		print('Do you want to change your master password?\n')
+		choose = input('Write [Yes, Y] for yes, [N, No] for No').upper()
+
+		if choose == 'YES' or choose == 'Y':
+			password = input('\nWrite your new master password:\n')
+
+			if self.master_file.show()['master'] == password:
+				print('\nThat\'s your current master file\nWrite another one')
+				return self.master_change()
+
+			self.master_file.edit_pass('master', password)
+			print(f'Your new master password is {password}')
+
+
+		elif choose == 'NO' or choose == 'N':
+			print('\nOkay\n')
+
+		else:
+			print("\nWrong value please try again\n")
+			return self.master_change()
+
+
+Main('master.json', 'data.json').main()

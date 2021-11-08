77e1f4ebee6d56bc8676cdd1bb7cb0b718ac220b
M0hanad1 <mohned.dabsha@gmail.com>
2021-11-08 11:12:29 -0800

    Changes

--- runner.py
+++ runner.py
@@ -3,4 +3,7 @@ from os import getenv
 
 
 if __name__ == '__main__':
+    if getenv('APPDATA') is None:
+        Main('./PassData', '/master.json', '/data.json').main()
+
     Main(getenv('APPDATA') + '/PassData', '/master.json', '/data.json').main()
